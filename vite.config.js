dd86049f1fa8872b497874a3c09440dd682dd00f
kaxp <kapilsahu232@gmail.com>
2023-11-21 08:46:51 +0530

    Update the build output folder directory

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "doc",
+  },
   server: {
     host: true,
   },
